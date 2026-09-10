f5d012ce35df205bf237267943f28ca4137db14d
Momen Hussein <morenocoder@gmail.com>
2026-09-11 00:18:01 +0300

    Added rest FBV for GET PUT DELETE

--- project/urls.py
+++ project/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("django/jsonresponsenomodel/", views.no_rest_no_model),
     #2
     path("django/jsonresponsefrommodel/", views.no_rest_from_model),
-    #3.1
-    path("rest/fbvlist/", views.fbv_list)
+    #3.1 GET POST
+    path("rest/fbvlist/", views.fbv_list),
+    #3.2 GET PUT DELETE
+    path("rest/fbvlist/<int:pk>", views.fbv_pk),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -43,10 +43,34 @@ def fbv_list(request):
         guests = Guest.objects.all()
         serializer = GuestSerializer(guests, many=True)
         return Response(serializer.data)
-    #POST
+    # POST
     elif request.method == 'POST':
         serializer = GuestSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+#3.2 GET PUT DELETE
+@api_view(['GET', 'PUT', 'DELETE'])
+def fbv_pk(request, pk):
+    try:
+        guest = Guest.objects.get(pk=pk)
+    except Guest.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # GET
+    if request.method == 'GET':
+        serializer = GuestSerializer(guest)
+        return Response(serializer.data)
+    # PUT
+    elif request.method == 'PUT':
+        serializer = GuestSerializer(guest, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    # DELETE
+    elif request.method == 'DELETE':
+        guest.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
