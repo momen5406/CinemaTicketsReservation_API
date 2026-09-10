20840d757294cf02bcfd6222b92366a138291817
Momen Hussein <morenocoder@gmail.com>
2026-09-10 22:38:45 +0300

    Added view without rest and no model query (FBV)

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,20 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+#1 Without REST and no model query FBV
+def no_rest_no_model(request):
+    guests = [
+        {
+            'id': 1,
+            'name': 'Ahmed',
+            'mobile': '0123456789'
+        },
+        {
+            'id': 2,
+            'name': 'Omar',
+            'mobile': '0268101214'
+        }
+    ]
+
+    return JsonResponse(guests, safe=False)
